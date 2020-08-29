fe635615c6694d386bdf4e07a5dd4840e552cedc
thudsonbu <hudson_thom@bentley.edu>
2020-08-29 00:06:31 -0400

    Fixed mobile spacing issue

--- src/styles/EventStyles.js
+++ src/styles/EventStyles.js
@@ -6,7 +6,7 @@ export default {
     Event: {
         width: "300px",
         height: "465px",
-        margin: "60px 50px 60px 50px",
+        margin: "60px 50px 90px 50px",
         transition: "box-shadow .25s",
         "&:hover": {
             cursor: "pointer",
@@ -14,6 +14,7 @@ export default {
         },
         [sizes.down("md")]: {
             margin: "50px",
+            marginBottom: "90px",
             width: "350px",
             boxShadow: "0px 0px 41px 12px rgba(194,194,194,1)",
         }
