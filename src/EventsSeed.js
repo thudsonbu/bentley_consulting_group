b2d973516668eb976a8172d20fccc8aeed25f53b
thudsonbu <hudson_thom@bentley.edu>
2020-08-29 00:00:55 -0400

    Event seed fix

--- src/EventsSeed.js
+++ src/EventsSeed.js
@@ -1,28 +1,24 @@
-import team from './images/team.jpg'
+import pizza from './images/PizzaConsulting.PNG';
+import citymadness from './images/CityMadness.jpg';
+import BPM from './images/BPM.PNG';
 
 
 export default [
     {
-        image: team,
-        title: "Projects",
+        image: pizza,
+        title: "Pizza + Consulting",
         date: "August 28, 2020",
         description: "Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or other type of compatible computer."
     },
     {
-        image: team,
-        title: "Projects",
+        image: citymadness,
+        title: "City Madness",
         date: "August 28, 2020",
         description: "Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or other type of compatible computer."
     },
     {
-        image: team,
-        title: "Projects",
-        date: "August 28, 2020",
-        description: "Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or other type of compatible computer."
-    },
-    {
-        image: team,
-        title: "Projects",
+        image: BPM,
+        title: "Bus Process Modeling",
         date: "August 28, 2020",
         description: "Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or other type of compatible computer."
     },
