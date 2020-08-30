c9f3fae344e7580587f5d9800210e7f3803ac06b
thudsonbu <hudson_thom@bentley.edu>
2020-08-30 19:34:20 -0400

    First release of team page

--- src/Team.js
+++ src/Team.js
@@ -15,11 +15,23 @@ class Team extends Component {
         const { classes } = this.props;
         const team = Team.map((member) => (
             <Member
-                
+                headshot={member.headshot}
+                name={member.name}
+                position={member.position}
+                email={mamber.email}
+                linkedin={member.linkedin}
+            />
         ))
         return(
             <div className={classes.Team}>
-
+                <div className={classes.SectionTitleBlock}>
+                    <h1 className={classes.SectionTitle}>Executive Board</h1>
+                </div>
+                <div className={classes.SectionSubTitle}>
+                    <h1>Meet Our Executive Board</h1>
+                    <p>The leaders of the Bentley Consulting Group are devoted to the organization and seeing our consultant thrive.</p>
+                </div>
+                {team}
             </div>
         )
     }
